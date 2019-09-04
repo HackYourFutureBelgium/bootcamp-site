9d861ced4bcd93b185745ad14c85dd4981fc2f4b
Michiel Leyman <michiel.leyman@gmail.com>
2019-09-04 17:44:28 +0200

    Remove unnecessary Gatsby boilerplate

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <p>Hello world</p>
+  </Layout>
+);
+
+export default IndexPage;
