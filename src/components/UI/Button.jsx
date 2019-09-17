9c2d64f269f4eac05eba1e4d7493e6d8474c7797
Michiel Leyman <michiel.leyman@gmail.com>
2019-09-17 11:03:12 +0200

    Change dynamic button sizing

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,37 +1,22 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { colors } from '../../styles/constants';
 
-const StyledButton = styled.button`
+const Button = styled.button`
   background-color: ${props => (props.inverted ? 'inherit ' : colors.orange)};
   color: ${props => (props.inverted ? colors.orange : '#fff')};
   border: ${props => (props.inverted ? `2px solid ${colors.orange}` : 'none')};
   border-radius: 3.2rem 0;
-  font-size: 1.4rem;
-  padding: 1.6rem 2.2rem;
+  font-size: ${props =>
+    (props.big && '1.6rem') || (props.medium && '1.4rem') || (props.small && '1.2rem')};
+  padding: ${props =>
+    (props.big && '1.8rem 2.5rem') ||
+    (props.medium && '1.6rem 2.2rem') ||
+    (props.small && '1.2rem 1.8rem')};
   :hover {
     cursor: pointer;
   }
 `;
-
-const BigButton = styled(StyledButton)`
-  font-size: 1.6rem;
-  padding: 1.8rem 2.5rem;
-`;
-
-const SmallButton = styled(StyledButton)`
-  font-size: 1.2rem;
-  padding: 1.2rem 1.8rem;
-`;
-
-const Button = ({ big, medium, small, children, ...rest }) => {
-  let ButtonComponent = StyledButton;
-  if (big) ButtonComponent = BigButton;
-  else if (small) ButtonComponent = SmallButton;
-
-  return <ButtonComponent {...rest}>{children}</ButtonComponent>;
-};
 
 Button.defaultProps = {
   inverted: false,
