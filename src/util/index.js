c3773bcbdcda3a8478ebe05cbf31523de531007e
Michiel Leyman <michiel.leyman@gmail.com>
2019-09-24 15:59:58 +0200

    Satisfy eslint

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,4 @@
 /* util folder is for standalone functions used over multiple components */
 
+// eslint-disable-next-line import/prefer-default-export
 export { default as flattenQueriedJson } from './flattenQueriedJson';
