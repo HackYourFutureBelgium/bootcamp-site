b414956a8fd73dec55129918ea4ed17774a3b642
Michiel Leyman <michiel.leyman@gmail.com>
2019-09-27 15:33:59 +0200

    Dynamically create project pages from CMS data

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const slugify = require('slugify');
+
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === 'build-html') {
     actions.setWebpackConfig({
@@ -11,4 +14,44 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       }
     });
   }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const result = await graphql(`
+    query {
+      allProjectJson {
+        edges {
+          node {
+            crest
+            name
+            date
+            description
+            partners
+            team
+            github
+          }
+        }
+      }
+    }
+  `);
+
+  result.data.allProjectJson.edges.forEach(({ node }) => {
+    const { name, date } = node;
+    const slug = slugify(name, { replacement: '-', lower: true });
+
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = `0${d.getMonth() + 1}`.slice(-2);
+
+    createPage({
+      path: `/projects/${year}/${month}/${slug}`,
+      matchPath: `/projects/${year}/${month}/${slug}`,
+      component: path.resolve(`./src/templates/ProjectDetail.jsx`),
+      context: {
+        slug,
+        ...node
+      }
+    });
+  });
 };
