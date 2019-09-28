9e55f8a536075501d4d9ddfcde5155565397f7dd
Michiel Leyman <michiel.leyman@gmail.com>
2019-09-28 22:05:50 +0200

    Add base page structure for project detail page

--- src/templates/ProjectDetail.jsx
+++ src/templates/ProjectDetail.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
-import Layout from '../components/Layout';
-import SEO from '../components/SEO';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
 
-const ProjectDetail = () => (
-  <Layout>
-    {/* <SEO title="" /> */}
-    <h1>Project details</h1>
-  </Layout>
-);
+import SEO from '../components/SEO';
+import Layout from '../components/Layout';
+import { colors } from '../styles/constants';
+import { projectType } from '../types';
+
+const PageContainer = styled(Layout)`
+  width: 85%;
+  margin: 0 auto;
+  h2 {
+    font-size: 1.7rem;
+  }
+`;
+
+const TopSection = styled.div`
+  margin-top: 3rem;
+  width: 100%;
+  > a {
+    color: ${colors.purple};
+  }
+`;
+
+const ProjectSection = styled.section`
+  width: 100%;
+`;
+
+const Team = styled.section`
+  width: 100%;
+`;
+
+const ProjectDetail = ({ pageContext: project }) => {
+  return (
+    <PageContainer>
+      {/* <SEO title={`${project.name} project`} /> */}
+      <TopSection>
+        <Link to="/projects">&lt; Our projects</Link>
+        <ProjectSection>
+          <h2>{project.name}</h2>
+        </ProjectSection>
+      </TopSection>
+      <Team>
+        <h2>
+          Meet the <strong>team</strong>
+        </h2>
+      </Team>
+    </PageContainer>
+  );
+};
+
+ProjectDetail.propTypes = {
+  pageContext: PropTypes.shape({ ...projectType }).isRequired
+};
 
 export default ProjectDetail;
