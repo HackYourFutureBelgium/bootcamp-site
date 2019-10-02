a5184efad2a1aac216ff52db8a1bc69ad8386169
Michiel Leyman <michiel.leyman@gmail.com>
2019-10-02 15:59:44 +0200

    Remove unused import

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { colors, dimensions } from '../../styles/constants';
+import { colors } from '../../styles/constants';
 
 const StyledCard = styled.article`
   background: #fff;
