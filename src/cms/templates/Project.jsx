c5e1d82a9e90b96d50422c924c2389f834a4ed04
Michiel Leyman <michiel.leyman@gmail.com>
2019-12-03 13:37:56 +0100

    Deal with empty partner and team arrays

--- src/cms/templates/Project.jsx
+++ src/cms/templates/Project.jsx
@@ -6,17 +6,21 @@ import Project from '../../templates/ProjectDetail';
 const ProjectPreview = ({ entry, fieldsMetaData }) => {
   const { data } = entry.toJS();
 
-  const partners = data.partners.reduce((all, p) => {
-    const partner = fieldsMetaData.getIn(['partners', 'partner', p]);
-    if (partner) all.push(partner.toJS());
-    return all;
-  }, []);
+  const partners = data.partners
+    ? data.partners.reduce((all, p) => {
+        const partner = fieldsMetaData.getIn(['partners', 'partner', p]);
+        if (partner) all.push(partner.toJS());
+        return all;
+      }, [])
+    : [];
 
-  const team = data.team.reduce((all, p) => {
-    const person = fieldsMetaData.getIn(['team', 'person', p]);
-    if (person) all.push(person.toJS());
-    return all;
-  }, []);
+  const team = data.team
+    ? data.team.reduce((all, p) => {
+        const person = fieldsMetaData.getIn(['team', 'person', p]);
+        if (person) all.push(person.toJS());
+        return all;
+      }, [])
+    : [];
 
   return (
     <StyleInjector>
